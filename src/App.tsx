ad55e430ad2dfd16713ba456c0b237ffe8ec1a15
Campeach-RD <campingsrd@gmail.com>
2026-05-28 19:46:40 -0400

    feat: scaffold Milaringo React app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="boot-screen">
+      <h1>Milaringo</h1>
+      <p>Entrenamiento avanzado en anestesiologia.</p>
+    </main>
+  );
+}
